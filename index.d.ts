0709e8e6513f1dcc84e21d015aabc1d7979de397
Alexandre Beaujour <abeaujour@steamulo.com>
2020-05-11 21:21:28 +0100

    fix https://github.com/Microsoft/TypeScript/wiki/Breaking-Changes#arbitrary-expressions-are-forbidden-in-export-assignments-in-ambient-contexts

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,7 @@
 declare module 'get-sport-result' {
-  export default {
-    options: object,
-    get(sport: string, name: string, date: Date, option?: { supplier: string, minRating: number }): Promise<any>
-  }
+    const module: {
+        options: object,
+        get(sport: string, name: string, date: Date, option?: { supplier: string, minRating: number }): Promise<any>
+    };
+    export default module;
 }
